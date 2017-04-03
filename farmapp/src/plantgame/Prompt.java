b9ef475ae951cd9e1b9ea503545e34933eb01acb
kanomtang <rungroj_s@cmu.ac.th>
2017-04-03 22:29:01 +0700

    design outer loop

--- farmapp/src/plantgame/Prompt.java
+++ farmapp/src/plantgame/Prompt.java
@@ -1,6 +1,7 @@
 package plantgame;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 
@@ -10,6 +11,34 @@ public class Prompt {
 		// TODO Auto-generated method stub
 	
 		 ArrayList<Plant> plantlist = new ArrayList<Plant>();
+		 Plant beg1,beg2,beg3,beg4 ;
+		int checkpoint= 0 ;
+		do{
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			
+			Scanner input = new Scanner(System.in);
+			System.out.println("please choose option by pressing the number "+input);
+			if(checkpoint==1){
+				
+			}else if (checkpoint==2){
+				
+			}else if (checkpoint==3){
+				
+			}else if (checkpoint==4){
+				
+			}else if (checkpoint==5){
+				
+			}else if (checkpoint==6){
+				
+			}
+		}while(checkpoint!=7);
 	}
 
 }
