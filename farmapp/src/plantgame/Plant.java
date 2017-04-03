a9784da0fc0b95e898ef0f6753fc01e22fc84ab7
kanomtang <rungroj_s@cmu.ac.th>
2017-04-03 23:03:52 +0700

    Adding view method

--- farmapp/src/plantgame/Plant.java
+++ farmapp/src/plantgame/Plant.java
@@ -3,5 +3,25 @@ package plantgame;
 import java.util.ArrayList;
 
 public class Plant {
+	private String plantName="null",plantFruit="null";
+	private int contAge=0 ,fullAge=0,contPlantWater=0,fullPlantWater=0;
+	private ArrayList<Plant> plantlist = new ArrayList<Plant>();
 	
+	public  void viewPlant(){
+		System.out.println(plantName);
+		System.out.println("   Age:[seed | seedling | mature | dead] ("+contAge+"//"+fullAge+" Days)");
+		System.out.println("   Health:("+contPlantWater+"//"+fullPlantWater+") (more hearths, "
+				+ "longer it can live without water)");
+		System.out.println("   Water:("+contAge+"//"+fullAge+") ");
+		System.out.println("   Fruit:"+ plantFruit);
+	}
+	public void plantSeed(){
+		
+	}public void waterPlant(){
+		
+	}public void plantSleep(){
+		
+	}
+	
+
 }
