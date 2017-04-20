de8a100e0cae2d1c0fdd67719a4e002fc397b77d
Buri <beeforce@hotmail.com>
2017-04-20 20:17:16 +0700

    GH-21
    empty class test

---
+++ farmapp/test/sleepless/farmapp/model/Plant/TestemptyPlant.java
@@ -0,0 +1,18 @@
+package sleepless.farmapp.model.Plant;
+
+public interface TestemptyPlant {
+
+	public interface Plant {
+
+
+		
+		
+		public  void viewPlant();
+		public void plantSeed();
+		public void waterPlant();
+		public void plantSleep(Plant p);
+		public String getPlantName();
+
+	}
+
+}
