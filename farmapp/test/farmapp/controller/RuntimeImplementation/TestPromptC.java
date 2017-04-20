d59de6fa480f95edfe16ad2353a223f7299719e7
kanomtang <rungroj_s@cmu.ac.th>
2017-04-20 22:30:35 +0700

    GH-23 setup the class and junit class for testing

---
+++ farmapp/test/farmapp/controller/RuntimeImplementation/TestPromptC.java
@@ -0,0 +1,15 @@
+package farmapp.controller.RuntimeImplementation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPromptC {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		// I don't know how to test in each case inside the loop
+	}
+
+}
