b2f1768c8400eaa1f63bbbf75565f7ff50438663
Buri <beeforce@hotmail.com>
2017-04-20 20:29:35 +0700

    GH-21
    fix error TestemptyPlant

--- farmapp/test/sleepless/farmapp/model/Plant/TestemptyPlant.java
+++ farmapp/test/sleepless/farmapp/model/Plant/TestemptyPlant.java
@@ -1,18 +1,60 @@
 package sleepless.farmapp.model.Plant;
 
-public interface TestemptyPlant {
+import sleepless.farmapp.model.PlantList.Plant;
 
-	public interface Plant {
+public class TestemptyPlant implements Plant {
 
-
+	private String plantName="null";
+	private int contAge=0 ,fullAge=0,contPlantWater=0,fullPlantWater=0,contHealth=0,fullHealth=0;
+	//private String[] plantFruit = new String [1];
+		private String[] plantFruit ={"Empty"};
+	
+	
+	
+	public void viewPlant() {
+		// TODO Auto-generated method stub
+		System.out.println(plantName);
+		System.out.println("   Age:[seed | seedling | mature | dead] ("+contAge+"//"+fullAge+" Days)");
+		System.out.println("   Health:("+contHealth+"//"+fullHealth+") (more hearths, "
+				+ "longer it can live without water)");
+		System.out.println("   Water:("+contAge+"//"+fullAge+") ");
+		for(int i = 0;i<plantFruit.length;i++){
+		System.out.println("   Fruit:"+ plantFruit[i]);	
+		}
 		
-		
-		public  void viewPlant();
-		public void plantSeed();
-		public void waterPlant();
-		public void plantSleep(Plant p);
-		public String getPlantName();
-
 	}
+
+	public void plantSeed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void waterPlant() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void plantSleep(Plant p) {
+		// TODO Auto-generated method stub
+		contAge++;
+		contPlantWater--;
+		if(contPlantWater==0){
+			contHealth--;
+		}
+		if(contHealth==0){
+			System.out.println("Peach died now");
+			p = null;
+			
+		}
+		
+	}
+
+	public String getPlantName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 
 }
